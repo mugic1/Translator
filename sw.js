95d37ef0733e2e7eb93f5b963b874de3fe97f6cd
mugic1 <dasandrew047@gmail.com>
2025-04-21 18:40:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/logo.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
   '/manifest.json'
 ];
 
